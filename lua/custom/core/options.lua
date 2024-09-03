e92ee6f7269860aa58e0c1bc7ca2e7c43d351c66
Mohammed Alrefai <mohammed@refam.io>
2024-09-04 02:39:35 +0300

    feat: make neotree always visible

---
+++ lua/custom/core/options.lua
@@ -0,0 +1,15 @@
+-- Make Neotree always visible
+--
+-- references:
+-- https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/679
+--
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  group = vim.api.nvim_create_augroup("NeotreeOnOpen", { clear = true }),
+  once = true,
+  callback = function(_)
+    if vim.fn.argc() == 0 then
+      vim.cmd("Neotree")
+    end
+  end,
+})
