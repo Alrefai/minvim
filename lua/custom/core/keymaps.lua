21b303ade92c2abdaaa45947f8378916cf6d51e0
Mohammed Alrefai <mohammed@refam.io>
2024-09-04 02:46:03 +0300

    feat: manage window splitting with keymaps

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -4,3 +4,9 @@ vim.keymap.set({ 'n', 'x' }, 's', '<Nop>')
 
 -- Misc
 vim.keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
+
+-- WINDOW MANAGEMENT
+vim.keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split [W]indow [V]ertically" })
+vim.keymap.set("n", "<leader>wh", "<C-w>s", { desc = "Split [W]indow [H]orizontally" })
+vim.keymap.set("n", "<leader>we", "<C-w>=", { desc = "Make Splits [W]indow [E]qual Size" })
+vim.keymap.set("n", "<leader>x", "<cmd>close<CR>", { desc = "[X] Close Current Split [W]indow" })
