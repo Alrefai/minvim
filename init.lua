f993637c9709ee18eaead1b9488e618eac841a38
Mohammed Alrefai <mohammed@refam.io>
2024-09-13 16:22:36 +0300

    feat: add show lsp documentaion on hover keymap

--- init.lua
+++ init.lua
@@ -549,6 +549,9 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          -- Show documentation for what is under cursor
+          map('gh', vim.lsp.buf.hover, '[G]oto Documentation on [H]over')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
