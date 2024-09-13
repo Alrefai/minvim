71abd5e7579cf817c92dbfbe010f0592253da7d2
Mohammed Alrefai <mohammed@refam.io>
2024-09-13 16:57:47 +0300

    feat: customize telescope

--- init.lua
+++ init.lua
@@ -383,12 +383,27 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          path_display = { 'smart' },
+          mappings = {
+            i = {
+              ['<C-k>'] = require('telescope.actions').move_selection_previous, -- move to prev result
+              ['<C-j>'] = require('telescope.actions').move_selection_next, -- move to next result
+            },
+          },
+        },
+        pickers = {
+          find_files = {
+            file_ignore_patterns = { 'node_modules', '.git' },
+            hidden = true,
+          },
+        },
+        live_grep = {
+          file_ignore_patterns = { 'node_modules', '.git' },
+          additional_args = function(_)
+            return { '--hidden' }
+          end,
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
