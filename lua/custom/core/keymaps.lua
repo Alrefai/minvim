012aa5f90390568d86a887bcec01a0714dc86ce8
Mohammed Alrefai <mohammed@refam.io>
2024-09-13 16:58:22 +0300

    feat: add custom keymap

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -5,8 +5,13 @@ vim.keymap.set({ 'n', 'x' }, 's', '<Nop>')
 -- Misc
 vim.keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
 
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
 -- WINDOW MANAGEMENT
-vim.keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split [W]indow [V]ertically" })
-vim.keymap.set("n", "<leader>wh", "<C-w>s", { desc = "Split [W]indow [H]orizontally" })
-vim.keymap.set("n", "<leader>we", "<C-w>=", { desc = "Make Splits [W]indow [E]qual Size" })
-vim.keymap.set("n", "<leader>x", "<cmd>close<CR>", { desc = "[X] Close Current Split [W]indow" })
+vim.keymap.set('n', '<leader>wv', '<C-w>v', { desc = 'Split [W]indow [V]ertically' })
+vim.keymap.set('n', '<leader>wh', '<C-w>s', { desc = 'Split [W]indow [H]orizontally' })
+vim.keymap.set('n', '<leader>we', '<C-w>=', { desc = 'Make Splits [W]indow [E]qual Size' })
+vim.keymap.set('n', '<leader>x', '<cmd>close<CR>', { desc = '[X] Close Current Split [W]indow' })
+vim.keymap.set('n', 'x', '"_x', { desc = 'Delete single character without copying into register' })
