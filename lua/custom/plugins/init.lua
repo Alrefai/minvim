1134d0cffb2b45e87bef7c35cf6d5ceb05fddc5a
Mohammed Alrefai <mohammed@refam.io>
2024-09-16 01:19:54 +0300

    feat: customize tabs with bufferline plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -170,4 +170,22 @@ return {
       },
     },
   },
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    event = 'VeryLazy',
+    opts = {
+      options = {
+        mode = 'tabs',
+        diagnostics = 'nvim_lsp',
+        diagnostics_indicator = function(count, level)
+          local icon = level:match 'error' and ' ' or ' '
+          return ' ' .. icon .. count
+        end,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
 }
