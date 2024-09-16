fe704d762e8c81527ebc9906fd8c1581367bf295
Mohammed Alrefai <mohammed@refam.io>
2024-09-16 22:20:05 +0300

    feat: add render-markdown plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -212,4 +212,14 @@ return {
       },
     },
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+    ft = { 'markdown' },
+    opts = {},
+  },
 }
